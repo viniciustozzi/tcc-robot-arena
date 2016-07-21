6506081514e2472cf5193824429d8704cebfa489
Vinicius Tozzi <tozzivinicius@gmail.com>
2016-07-21 19:17:09 -0300

    changes

--- Assets/Scripts/Prototype/Blocks/BooleanExpression.cs
+++ Assets/Scripts/Prototype/Blocks/BooleanExpression.cs
@@ -4,39 +4,56 @@ using System;
 
 public class BooleanExpression
 {
-    private string m_var1;
-    private string m_var2;
+    private VariableInfo m_var1;
+    private VariableInfo m_var2;
     private BooleanOperator m_operationType;
 
-    public BooleanExpression(string var1, string var2, BooleanOperator opType)
+
+
+    public BooleanExpression(string var1Name, string var2Name, BooleanOperator opType)
 	{
-		m_var1 = var1;
-		m_var2 = var2;
+		m_var1 = VariableController.Variables[var1Name];
+        m_var2 = VariableController.Variables[var2Name];
+
         m_operationType = opType;
 	}
 
-    //TODO: Alterar para funcionar com variáveis e não com valores fixos!!!
+    /// <summary>
+    /// Retorna verdadeiro se var1 operation var2
+    /// </summary>
+    /// <param name="onParseError">O parâmetro é convertido para inteiro, se houver erro nessa conversão, é invocado um callback de erro</param>
+    public bool IsTrue(Action onParseError = null)
+    {
+        int value1 = 0;
+        int value2 = 0;
 
-	/// <summary>
-	/// Retorna verdadeiro se var1 operation var2
-	/// </summary>
-//    public bool IsTrue()
-//    {
-//		switch (m_operationType) 
-//		{
-//			case BooleanOperator.Bigger:
-//				return m_var1 > m_var2;
-//			case BooleanOperator.BiggerAndEqual:
-//				return m_var1 >= m_var2;
-//			case BooleanOperator.Equal:
-//				return m_var1 == m_var2;
-//			case BooleanOperator.LessAndEqual:
-//				return m_var1 <= m_var2;
-//			case BooleanOperator.Less:
-//				return m_var1 < m_var2;
-//		}
-//
-//		Debug.LogWarning("Expressão booleana não pode ser validada corretamente!");
-//		return false;
-//    }
+        try
+        {
+            //value1
+        }
+        catch (Exception e)
+        {
+            
+            throw;
+        }
+
+        //switch (m_operationType)
+        //{
+        //    case BooleanOperator.Bigger:
+        //        return m_var1.Value > m_var2.Value;
+        //    case BooleanOperator.BiggerAndEqual:
+        //        return m_var1 >= m_var2;
+        //    case BooleanOperator.Equal:
+        //        return m_var1 == m_var2;
+        //    case BooleanOperator.LessAndEqual:
+        //        return m_var1 <= m_var2;
+        //    case BooleanOperator.Less:
+        //        return m_var1 < m_var2;
+        //}
+
+        Debug.LogWarning("Expressão booleana não pode ser validada corretamente!");
+        return false;
+    }
+
+    
 }
