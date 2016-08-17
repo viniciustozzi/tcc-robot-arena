b3f4c866ecb662ba3bc8bf263007626a11ff2ed3
Vinicius Tozzi <tozzivinicius@gmail.com>
2016-08-16 22:39:33 -0300

    Adicionando blcocos

--- Assets/Scripts/UI/TabsController.cs
+++ Assets/Scripts/UI/TabsController.cs
@@ -1,9 +1,18 @@
 ﻿using UnityEngine;
 using System;
+using System.Collections.Generic;
 
 public class TabsController : MonoBehaviour
 {
     private CurrentBlocksAvaible m_currentBlocks;
+
+    public Transform scrollContent;
+
+    public List<GameObject> RobotActionsBlocks;
+    public List<GameObject> VariablesBlocks;
+    public List<GameObject> EventsBlocks;
+    public List<GameObject> OperatorsBlocks;
+    public List<GameObject> ControlsBlocks;
 
     public void SetBlockGroup(int group)
     {
@@ -14,12 +23,28 @@ public class TabsController : MonoBehaviour
         }
             
         m_currentBlocks = (CurrentBlocksAvaible)group;
-
-        Debug.Log("mcurretBLock: " + m_currentBlocks);
     }
 
     public void UpdateBlockGroup()
     {
+        switch (m_currentBlocks)
+        {
+            case CurrentBlocksAvaible.RobotActions:
 
+                foreach (var item in RobotActionsBlocks)
+                {
+
+                }
+
+                break;
+            case CurrentBlocksAvaible.Variables:
+                break;
+            case CurrentBlocksAvaible.Events:
+                break;
+            case CurrentBlocksAvaible.Operators:
+                break;
+            case CurrentBlocksAvaible.Controls:
+                break;
+        }
     }
 }
