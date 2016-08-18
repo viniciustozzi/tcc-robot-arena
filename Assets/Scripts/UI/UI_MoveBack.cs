55d6de32f1c7a7321ddc18f7aae2f3c8a39ebaf8
Vinicius Tozzi <tozzivinicius@gmail.com>
2016-08-18 20:53:00 -0300

    Bloco de moveback

---
+++ Assets/Scripts/UI/UI_MoveBack.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class UI_MoveBack : MonoBehaviour
+{
+    public InputField input_distance;
+
+    public int Distance
+    {
+        get
+        {
+            int distance = 0;
+
+            if (!int.TryParse(input_distance.text, out distance))
+                Debug.LogWarning("Erro ao converter distância do bloco MoveBack para inteiro");
+
+            return distance;
+        }
+    }
+}
