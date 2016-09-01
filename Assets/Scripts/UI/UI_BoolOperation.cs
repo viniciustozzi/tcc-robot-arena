8c4f9f95967c4348fd0b47ce18941bf3de83e372
Vinicius Tozzi <tozzivinicius@gmail.com>
2016-09-01 03:02:51 -0300

    Lógica de operações booleanas (relacionais e logicas)

---
+++ Assets/Scripts/UI/UI_BoolOperation.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class UI_BoolOperation : UIBlock
+{
+    public Transform m_horizontalGroup;
+
+    private List<UI_LogicalOperation> m_logicalOperations;
+    private UI_RelationalOperation m_relationalOp;
+
+    public override void OnDrop(PointerEventData eventData)
+    {
+        UI_LogicalOperation logicalBlock = eventData.pointerDrag.GetComponent<UI_LogicalOperation>();
+
+        if (logicalBlock != null)
+        {
+            m_logicalOperations.Add(logicalBlock);
+            logicalBlock.transform.SetParent(m_horizontalGroup);
+            logicalBlock.DropValid = true;
+        }
+    }
+}
---
+++ Assets/Scripts/UI/UI_LogicalOperation.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class UI_LogicalOperation : UIBlock
+{
+    public LogicalOperation op1;
+    public LogicalOperation op2;
+    public LogicalOperator opType;
+
+    
+}
