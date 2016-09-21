14118737d8837e7d76240c93bb906a45e3a26f34
Vinicius Tozzi <tozzivinicius@gmail.com>
2016-09-20 22:33:39 -0300

    Bloco para setar variável funcionando

--- Assets/Scripts/Blocks/SetVariable.cs
+++ Assets/Scripts/Blocks/SetVariable.cs
@@ -14,12 +14,38 @@ public class SetVariable : AbstractBlock
 
     public override void Initialize()
     {
-        
+
     }
 
     public override void Run(Action blockCallback)
     {
-        //TODO:  Tratar o tipo da variável!!!
-        VariableController.SetValue(VarName, Value);
+
+        VariableInfo varInfo = VariableController.Variables[VarName];
+
+        switch (varInfo.Type)
+        {
+            case VariableType.Number:
+                float floatValue = 0;
+                if (!float.TryParse(Value, out floatValue))
+                    Debug.LogWarning("Não foi possível converter o valor para float na hora de setar a variável!");
+
+                VariableController.SetValue(VarName, (float)floatValue);
+
+                break;
+            case VariableType.String:
+
+                VariableController.SetValue(VarName, Value);
+
+                break;
+            case VariableType.Bool:
+
+                bool boolValue = false;
+                if (!bool.TryParse(Value, out boolValue))
+                    Debug.LogWarning("Não foi possível converter o valor para bool na hora de setar a variável!");
+
+                VariableController.SetValue(VarName, (bool)boolValue);
+
+                break;
+        }
     }
 }
