679cfcf135b60bc1f78af15a0dcdc26f818b6aa2
Vinicius Tozzi <tozzivinicius@gmail.com>
2016-10-02 21:42:27 -0300

    Ajuste de operações lógicas ajustado

--- Assets/Scripts/UI/UI_LogicalOperation.cs
+++ Assets/Scripts/UI/UI_LogicalOperation.cs
@@ -11,6 +11,16 @@ public class UI_LogicalOperation : UIBlock
     public RelationalOpSpace opSpace2;
     public Dropdown drop_opType;
 
+    protected override void Awake()
+    {
+        setOperatorTypes();
+
+        base.Awake();
+    }
+
+    /// <summary>
+    /// Retorna a expressão lógica baseada nesse bloco
+    /// </summary>
     public LogicalOperation GetLogicalOperation()
     {
         LogicalOperator opType = (LogicalOperator)Enum.Parse(typeof(LogicalOperator), drop_opType.captionText.text);
@@ -20,6 +30,7 @@ public class UI_LogicalOperation : UIBlock
         return new LogicalOperation(op1, op2, opType);
     }
 
+    //Adiciona todos os operadores lógicos no dropdownlist 
     private void setOperatorTypes()
     {
         drop_opType.ClearOptions();
@@ -28,17 +39,22 @@ public class UI_LogicalOperation : UIBlock
 
         for (int i = 0; i < Enum.GetNames(typeof(LogicalOperator)).Length; i++)
         {
-            Dropdown.OptionData op = new Dropdown.OptionData(Enum.GetName(typeof(LogicalOperator), i));
+            Dropdown.OptionData op = new Dropdown.OptionData(getOperatorPrettyName((LogicalOperator)i));
             options.Add(op);
         }
 
         drop_opType.AddOptions(options);
     }
 
-    protected override void Awake()
+    private string getOperatorPrettyName(LogicalOperator logicalOperator)
     {
-        setOperatorTypes();
+        switch (logicalOperator)
+        {
+            case LogicalOperator.And: return "E";
+            case LogicalOperator.Or: return "OU";
+        }
 
-        base.Awake();
+        Debug.LogWarning("Tipo de operador lógico inválido no método: getOperatorPrettyName()");
+        return string.Empty;
     }
 }
