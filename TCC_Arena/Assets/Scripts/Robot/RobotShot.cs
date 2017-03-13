e721fe439f2da50fa124eb1f77e6822ca631481e
viniciustozzi <tozzivinicius@gmail.com>
2017-03-13 02:46:26 -0300

    Primeiro commit

---
+++ TCC_Arena/Assets/Scripts/Robot/RobotShot.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class RobotShot : MonoBehaviour
+{
+
+    public GameObject shotPrefab;
+    public Transform shotTransform;
+    public float cooldownTime;
+
+    private Action m_callback;
+
+    public void ShootCommand(float speed, Action onFinish)
+    {
+        m_callback = onFinish;
+
+        var shot = (GameObject)Instantiate(shotPrefab, shotTransform.position, Quaternion.identity);
+        ShotInfo info = shot.GetComponent<ShotInfo>();
+        info.SetInfo(speed);
+
+        Invoke("finishCooldownTime", cooldownTime);
+    }
+
+    private void finishCooldownTime()
+    {
+        if (m_callback != null)
+        {
+            m_callback.Invoke();
+        }
+    }
+
+
+}
---
+++ TCC_Arena/Assets/Scripts/ShotInfo.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody))]
+public class ShotInfo : MonoBehaviour
+{
+    private Rigidbody m_rigidbody;
+
+    public void SetInfo(float speed)
+    {
+        m_rigidbody = GetComponent<Rigidbody>();
+
+        m_rigidbody.velocity = transform.forward * speed;
+    }
+}
